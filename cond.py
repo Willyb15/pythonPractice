2aa1c898868de94a2abe7b5423889774c38bede2
Will Bryant <william.steen.bryant@gmail.com>
2018-05-01 14:06:59 -0600

    Adding conditional file practice

---
+++ cond.py
@@ -0,0 +1,43 @@
+# if (1==1):
+# 	print "True!!"
+# print "False"
+
+# if(1 == 2):
+# 	print "True!"
+# else:
+# 	print "False"
+
+
+# if(1 == 2):
+# 	print "True!"
+# elif(2 == 3):
+# 	print "Second one is true!"
+# else:
+# 	print "False"
+
+# teamSize = 5;
+# question = "How big is your team?"
+# print question
+# response = raw_input('>>>')
+# #Raw input always collected as string
+# response_asInteger = int(response)
+# if (response_asInteger != teamSize):
+# 	print "you must be thinking of the wrong team"
+# else:
+# 	print "You have the right team!"
+
+
+#Random is a python module, that comes with python
+# import random 
+# rand_number = random.randint(1,10)
+# print rand_number
+
+# loops
+# A while loop while run forever or until the condition is False
+
+keep_going = True
+while keep_going:
+	get_answer = raw_input('Please Hit any Key!!!')
+	keep_going = False
+print "You are no longer in the loop!"
+
