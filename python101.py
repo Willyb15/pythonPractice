bc578944744b5c75ab8d0ac15c9634e5ec12f52d
Will Bryant <william.steen.bryant@gmail.com>
2018-05-01 12:29:51 -0600

    First Commit from the command line

---
+++ python101.py
@@ -0,0 +1,150 @@
+# print "Hello World"
+# print ("Hello, WOrld")
+	# print "This
+	# 	is multiple lines
+	# 	haha!"
+# print """
+# 	Triple Quotes will make 
+# 	python Happy
+# 	Event though this is ugle
+# 	"""
+
+# variales - strings, letters, numbers, or any other stuff you acn make with a keyboard. 
+# # a variable is just a fast way to 
+# refer to something else. 
+# It DOES NOT make the computer faster. It makes it sloewr
+# # In python, there is no declation. It is dynamic. It exists when you make it. 
+
+ # Python is NOT heavily type.
+# # 
+theBestClassInDenver = "this guy"
+
+name ="Will Bryant"; #this is a string
+
+# Data Types:
+# strings - English stuff, for people to read.
+# Numbers - someting with digits and the stuff that goes with them (- or . or e)
+# Floats, integers
+# # -- float has a . in it
+# # -- integer - has no .
+# # Booleans - True or false, 1 or 0, off or on.
+# # list -list of things. A single variable with a bunch of like parts
+# # dictionary - variable of variable
+# # objects - we will deal with this later
+
+name = "Will" + "Bryant" # + is a concateante symbol with strings
+first_name = "Will"
+last_name = "Bryant"
+# full_name = first + " " + last;
+# print full_name
+# meaning_of_life = 40 + 2; # + i snow an addition symbol
+# forty_two = 84 / 2;
+# forty_two = 21 * 2;
+# one = 83 % 2;
+# print forty_two
+# # print "The meaning of life is " + forty_two
+
+
+# # Intermingle english and vars: way 1
+# print "Hello {} {}".format(first_name,last_name)
+# # # Way 2
+# print "Hello %s" % "Will"
+
+# # # Floats
+# pi = 3.14
+# print type(pi)
+
+# makeMeAnInt = int(pi);
+# print makeMeAnInt
+# print type(makeMeAnInt)
+# print .2+.1
+
+
+# variable = True
+# print type(variable)
+
+
+
+# # # Booleans
+# the_truth = True;
+# print type(the_truth)
+
+
+# addOneToMe = 1
+# print addOneToMe
+# addOneToMe = addOneToMe + 1
+# print addOneToMe
+# # addOneToMe++
+# # print addOneToMe
+# addOneToMe += 1
+# print addOneToMe
+# addOneToMe += 10
+# print addOneToMe
+# addOneToMe -= 10
+
+
+
+# Order of operations is what you learned in the 3rd grade.
+# will = 2 + 3 * 4 
+# print will
+# # = 14, not 24
+# x = 2 + 4 /2  
+# print x
+# = 4, not 3
+# () overrides everything. Use ()
+
+
+
+print "What is your name?"
+name = raw_input('>')
+print "Your name is %s" % name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
